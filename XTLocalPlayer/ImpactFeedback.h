e9cf6b963d1c97f08b386d00511009eec7917760
uponup <1030360567@qq.com>
2019-11-16 11:09:36 +0800

    git add .impact

---
+++ XTLocalPlayer/ImpactFeedback.h
@@ -0,0 +1,17 @@
+//
+//  ImpactFeedback.h
+//  XTLocalPlayer
+//
+//  Created by 龙格 on 2019/11/16.
+//  Copyright © 2019 Paul Gao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ImpactFeedback : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XTLocalPlayer/ImpactFeedback.m
@@ -0,0 +1,13 @@
+//
+//  ImpactFeedback.m
+//  XTLocalPlayer
+//
+//  Created by 龙格 on 2019/11/16.
+//  Copyright © 2019 Paul Gao. All rights reserved.
+//
+
+#import "ImpactFeedback.h"
+
+@implementation ImpactFeedback
+
+@end
